ab81ba2de30277975e48edb69fc5932f3fef5a57
Purvi Trivedi <purvi.trivedi@SAPPHIRE.NET>
2022-11-17 12:06:17 +0530

    front-end changes

--- public/akwaba/akwaba/config/db_pg.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = pg_connect("host=10.10.3.50  dbname=gis user=postgres password=uBj1vfYzg5Le3");
-if(!$db) {
-	
-    echo "Database Connection Failed";die;
-}
-?>
--- public/akwaba/akwaba/logout.php
+++
@@ -1,7 +0,0 @@
-<?php   
-session_start(); //to ensure you are using same session
-unset($_SESSION['users']);
-session_destroy(); //destroy the session
-header("location:http://localhost:8888/akwaba/public/akwaba/index.php"); //to redirect back to "index.php" after logging out
-exit();
-?>
